5e30c531f89a630d47d47d37566b69c6da876d08
Rodrigodcs <rodrigodcsee@gmail.com>
2022-06-22 10:18:05 -0300

    Preparando revisão de código

--- src/schemas/signInSchema.js
+++
@@ -1,8 +0,0 @@
-import joi from "joi"
-
-const signInSchema = joi.object({
-    email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
-    password: joi.string().required()
-})
-
-export default signInSchema;
